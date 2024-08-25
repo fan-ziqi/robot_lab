a7ab2a25f1838c34b210d6fea58674623b276981
fan-ziqi <fanziqi614@gmail.com>
2024-08-25 23:44:13 +0800

    fix class name

--- exts/robot_lab/robot_lab/tasks/locomotion/velocity/config/openloong/__init__.py
+++ exts/robot_lab/robot_lab/tasks/locomotion/velocity/config/openloong/__init__.py
@@ -11,7 +11,7 @@ gym.register(
     entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": rough_env_cfg.OpenLoong_RoughEnvCfg,
+        "env_cfg_entry_point": rough_env_cfg.OpenLoongOpenLoongRoughEnvCfg,
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:OpenLoongOpenLoongRoughPPORunnerCfg",
     },
 )
@@ -21,7 +21,7 @@ gym.register(
     entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": flat_env_cfg.OpenLoong_FlatEnvCfg,
+        "env_cfg_entry_point": flat_env_cfg.OpenLoongOpenLoongFlatEnvCfg,
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:OpenLoongOpenLoongFlatPPORunnerCfg",
     },
 )
