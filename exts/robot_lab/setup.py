c369f18fa159480eadde0729109b1aa4e3f4228b
fan-ziqi <fanziqi614@gmail.com>
2024-09-03 12:03:05 +0800

    fix: install_requires

--- exts/robot_lab/setup.py
+++ exts/robot_lab/setup.py
@@ -14,6 +14,9 @@ EXTENSION_TOML_DATA = toml.load(os.path.join(EXTENSION_PATH, "config", "extensio
 INSTALL_REQUIRES = [
     # NOTE: Add dependencies
     "psutil",
+    "lxml",
+    "transformations",
+    "pybullet",
 ]
 
 # Installation operation
