a17c10907ed8d63d7e1e472412d946ce165b50dd
fan-ziqi <fanziqi614@gmail.com>
2024-12-04 21:28:01 +0800

    fix g1 bug

--- exts/robot_lab/robot_lab/tasks/locomotion/velocity/config/unitree_g1/__init__.py
+++ exts/robot_lab/robot_lab/tasks/locomotion/velocity/config/unitree_g1/__init__.py
@@ -11,7 +11,7 @@ gym.register(
     entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": f"{__name__}:rough_env_cfg:G1RoughEnvCfg",
+        "env_cfg_entry_point": f"{__name__}.rough_env_cfg:G1RoughEnvCfg",
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:G1RoughPPORunnerCfg",
     },
 )
@@ -22,7 +22,7 @@ gym.register(
     entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": f"{__name__}:flat_env_cfg:G1FlatEnvCfg",
+        "env_cfg_entry_point": f"{__name__}.flat_env_cfg:G1FlatEnvCfg",
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:G1FlatPPORunnerCfg",
     },
 )
