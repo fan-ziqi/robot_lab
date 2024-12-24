65b9882b110f9a6c2f9edf54c01c372d5efbc9d8
fan-ziqi <fanziqi614@gmail.com>
2024-12-25 00:01:42 +0800

    fix: del actions

--- exts/robot_lab/robot_lab/tasks/locomotion/velocity/mdp/__init__.py
+++ exts/robot_lab/robot_lab/tasks/locomotion/velocity/mdp/__init__.py
@@ -3,5 +3,4 @@
 from omni.isaac.lab.envs.mdp import *  # noqa: F401, F403
 from omni.isaac.lab_tasks.manager_based.locomotion.velocity.mdp import *  # noqa: F401, F403
 
-from .actions import *  # noqa: F401, F403
 from .rewards import *  # noqa: F401, F403
