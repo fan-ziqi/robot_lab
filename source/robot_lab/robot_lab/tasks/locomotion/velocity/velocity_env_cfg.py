d43b88a9109c60a9c94e300e2b98232acd8b2e5e
fan-ziqi <fanziqi614@gmail.com>
2025-03-18 17:03:46 +0800

    update friction_range

--- source/robot_lab/robot_lab/tasks/locomotion/velocity/velocity_env_cfg.py
+++ source/robot_lab/robot_lab/tasks/locomotion/velocity/velocity_env_cfg.py
@@ -260,8 +260,8 @@ class EventCfg:
         mode="startup",
         params={
             "asset_cfg": SceneEntityCfg("robot", body_names=".*"),
-            "static_friction_range": (0.1, 2.25),
-            "dynamic_friction_range": (0.1, 1.0),
+            "static_friction_range": (0.1, 1.0),
+            "dynamic_friction_range": (0.1, 0.8),
             "restitution_range": (0.0, 0.5),
             "num_buckets": 32,
         },
