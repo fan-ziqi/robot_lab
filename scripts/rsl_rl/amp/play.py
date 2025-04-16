ed3a172b6ccf6d748e0f6e22d19f7ae1cb3aab5f
fan-ziqi <fanziqi614@gmail.com>
2025-04-16 11:38:14 +0800

    fix: extract the neural network module

--- scripts/rsl_rl/amp/play.py
+++ scripts/rsl_rl/amp/play.py
@@ -153,22 +153,31 @@ def main():
     # obtain the trained policy for inference
     policy = ppo_runner.get_inference_policy(device=env.unwrapped.device)
 
+    # extract the neural network module
+    # we do this in a try-except to maintain backwards compatibility.
+    try:
+        # version 2.3 onwards
+        policy_nn = ppo_runner.alg.policy
+    except AttributeError:
+        # version 2.2 and below
+        policy_nn = ppo_runner.alg.actor_critic
+
     # export policy to onnx/jit
     export_model_dir = os.path.join(os.path.dirname(resume_path), "exported")
     export_policy_as_onnx(
-        actor_critic=ppo_runner.alg.actor_critic,
+        policy=policy_nn,
         normalizer=ppo_runner.obs_normalizer,
         path=export_model_dir,
         filename="policy.onnx",
     )
     export_policy_as_jit(
-        actor_critic=ppo_runner.alg.actor_critic,
+        policy=policy_nn,
         normalizer=ppo_runner.obs_normalizer,
         path=export_model_dir,
         filename="policy.pt",
     )
 
-    dt = env.unwrapped.physics_dt
+    dt = env.unwrapped.step_dt
 
     # reset environment
     obs, _ = env.get_observations()
