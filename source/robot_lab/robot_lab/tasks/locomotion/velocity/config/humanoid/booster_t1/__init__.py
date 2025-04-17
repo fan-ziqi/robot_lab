c4eb6a4126733f97e7e3ec23a03af42383c65356
robotsfan <fanziqi614@gmail.com>
2025-04-17 13:26:51 +0000

    pre-commit

--- source/robot_lab/robot_lab/tasks/locomotion/velocity/config/humanoid/booster_t1/__init__.py
+++ source/robot_lab/robot_lab/tasks/locomotion/velocity/config/humanoid/booster_t1/__init__.py
@@ -1,20 +1,6 @@
-# Copyright (c) 2024-2025 Ziqi Fan
-# SPDX-License-Identifier: Apache-2.0
-
 import gymnasium as gym
 
 from . import agents
-
-gym.register(
-    id="RobotLab-Isaac-Velocity-Rough-Booster-T1-v0",
-    entry_point="isaaclab.envs:ManagerBasedRLEnv",
-    disable_env_checker=True,
-    kwargs={
-        "env_cfg_entry_point": f"{__name__}.rough_env_cfg:BoosterT1RoughEnvCfg",
-        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:BoosterT1RoughPPORunnerCfg",
-    },
-)
-
 
 gym.register(
     id="RobotLab-Isaac-Velocity-Flat-Booster-T1-v0",
@@ -23,5 +9,15 @@ gym.register(
     kwargs={
         "env_cfg_entry_point": f"{__name__}.flat_env_cfg:BoosterT1FlatEnvCfg",
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:BoosterT1FlatPPORunnerCfg",
+    },
+)
+
+gym.register(
+    id="RobotLab-Isaac-Velocity-Rough-Booster-T1-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.rough_env_cfg:BoosterT1RoughEnvCfg",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:BoosterT1RoughPPORunnerCfg",
     },
 )
