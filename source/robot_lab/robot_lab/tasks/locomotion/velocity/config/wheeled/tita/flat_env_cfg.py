1e790ff44b6a98b33a4b38b9e26646595c09dc2d
yankai <1924286463@qq.com>
2025-07-22 14:29:51 +0800

    Update flat_env_cfg.py

--- source/robot_lab/robot_lab/tasks/locomotion/velocity/config/wheeled/tita/flat_env_cfg.py
+++ source/robot_lab/robot_lab/tasks/locomotion/velocity/config/wheeled/tita/flat_env_cfg.py
@@ -4,8 +4,24 @@
 from isaaclab.utils import configclass
 
 from .rough_env_cfg import DDTTitaRoughEnvCfg
+import isaaclab.terrains as terrain_gen
 
-
+COBBLESTONE_ROAD_CFG = terrain_gen.TerrainGeneratorCfg(
+    size=(8.0, 8.0),
+    border_width=20.0,
+    num_rows=9,
+    num_cols=21,
+    horizontal_scale=0.1,
+    vertical_scale=0.005,
+    slope_threshold=0.75,
+    difficulty_range=(0.0, 1.0),
+    use_cache=False,
+    sub_terrains={
+        "random_rough": terrain_gen.HfRandomUniformTerrainCfg(
+            proportion=0.2, noise_range=(0.02, 0.10), noise_step=0.02, border_width=0.25
+        ),
+    },
+)
 @configclass
 class DDTTitaFlatEnvCfg(DDTTitaRoughEnvCfg):
     def __post_init__(self):
@@ -15,8 +31,8 @@ class DDTTitaFlatEnvCfg(DDTTitaRoughEnvCfg):
         # override rewards
         self.rewards.base_height_l2.params["sensor_cfg"] = None
         # change terrain to flat
-        self.scene.terrain.terrain_type = "plane"
-        self.scene.terrain.terrain_generator = None
+        self.scene.terrain.terrain_type = "generator"
+        self.scene.terrain.terrain_generator = COBBLESTONE_ROAD_CFG
         # no height scan
         self.scene.height_scanner = None
         self.observations.policy.height_scan = None
