51133b6972772f53d9a5c99d33cc627d2ae6a34f
fan-ziqi <fanziqi614@gmail.com>
2025-08-01 13:55:14 +0800

    feat: bump version to v2.2.0

--- source/robot_lab/setup.py
+++ source/robot_lab/setup.py
@@ -42,8 +42,9 @@ setup(
     classifiers=[
         "Natural Language :: English",
         "Programming Language :: Python :: 3.10",
-        "Isaac Sim :: 2023.1.1",
-        "Isaac Sim :: 4.0.0",
+        "Programming Language :: Python :: 3.11",
+        "Isaac Sim :: 4.5.0",
+        "Isaac Sim :: 5.0.0",
     ],
     zip_safe=False,
 )
