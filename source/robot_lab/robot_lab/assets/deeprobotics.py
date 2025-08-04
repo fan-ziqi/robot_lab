8839b5d622cbd6a3397b9d2e2c7e0c811849c77a
fan-ziqi <fanziqi614@gmail.com>
2025-08-04 22:31:13 +0800

    fix: delete deeprobotics x30 robot asset

--- source/robot_lab/robot_lab/assets/deeprobotics.py
+++ source/robot_lab/robot_lab/assets/deeprobotics.py
@@ -13,62 +13,6 @@ from robot_lab.assets.utils.usd_converter import (  # noqa: F401
     xacro_to_usd,
 )
 
-DEEPROBOTICS_X30_CFG = ArticulationCfg(
-    spawn=sim_utils.UsdFileCfg(
-        func=spawn_from_lazy_usd,
-        usd_path=urdf_to_usd(  # type: ignore
-            file_path=f"{ISAACLAB_ASSETS_DATA_DIR}/Robots/deeprobotics/x30_description/urdf/x30.urdf",
-            output_usd_path=f"{ISAACLAB_ASSETS_DATA_DIR}/Robots/deeprobotics/x30_description/usd/x30.usd",
-            merge_joints=True,
-            fix_base=False,
-        ),
-        activate_contact_sensors=True,
-        rigid_props=sim_utils.RigidBodyPropertiesCfg(
-            disable_gravity=False,
-            retain_accelerations=False,
-            linear_damping=0.0,
-            angular_damping=0.0,
-            max_linear_velocity=1000.0,
-            max_angular_velocity=1000.0,
-            max_depenetration_velocity=1.0,
-        ),
-        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
-            enabled_self_collisions=False, solver_position_iteration_count=4, solver_velocity_iteration_count=0
-        ),
-    ),
-    init_state=ArticulationCfg.InitialStateCfg(
-        pos=(0.0, 0.0, 0.5),
-        joint_pos={
-            ".*HipX_joint": 0.0,
-            ".*HipY_joint": -0.8,
-            ".*Knee_joint": 1.6,
-        },
-        joint_vel={".*": 0.0},
-    ),
-    soft_joint_pos_limit_factor=0.9,
-    actuators={
-        "Hip": DCMotorCfg(
-            joint_names_expr=[".*_Hip[X,Y]_joint"],
-            effort_limit=84.0,
-            saturation_effort=84.0,
-            velocity_limit=17.5,
-            stiffness=80.0,
-            damping=3.0,
-            friction=0.0,
-        ),
-        "Knee": DCMotorCfg(
-            joint_names_expr=[".*_Knee_joint"],
-            effort_limit=150.0,
-            saturation_effort=150.0,
-            velocity_limit=16.1,
-            stiffness=80.0,
-            damping=3.0,
-            friction=0.0,
-        ),
-    },
-)
-
-
 DEEPROBOTICS_LITE3_CFG = ArticulationCfg(
     spawn=sim_utils.UsdFileCfg(
         func=spawn_from_lazy_usd,
