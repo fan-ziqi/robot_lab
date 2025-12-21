00ef99a09332987d9c45a898a3991197b83ea543
fengsvkn <105400854+fengsvkn@users.noreply.github.com>
2025-12-22 00:49:20 +0800

    Add BDX humanoid velocity configs

---
+++ source/robot_lab/robot_lab/assets/bdx.py
@@ -0,0 +1,68 @@
+# Copyright (c) 2024-2025 Ziqi Fan
+# SPDX-License-Identifier: Apache-2.0
+
+"""Configuration for BDX humanoid robot."""
+
+import isaaclab.sim as sim_utils
+from isaaclab.actuators import ImplicitActuatorCfg
+from isaaclab.assets.articulation import ArticulationCfg
+
+from robot_lab.assets import ISAACLAB_ASSETS_DATA_DIR
+
+##
+# Configuration
+##
+
+
+BDX_ACTION_SCALE = 0.25
+
+BDX_CFG = ArticulationCfg(
+    spawn=sim_utils.UrdfFileCfg(
+        fix_base=False,
+        merge_fixed_joints=True,
+        replace_cylinders_with_capsules=False,
+        asset_path=f"{ISAACLAB_ASSETS_DATA_DIR}/Robots/bdx/bdx_description/urdf/bdx.urdf",
+        activate_contact_sensors=True,
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            retain_accelerations=False,
+            linear_damping=0.0,
+            angular_damping=0.0,
+            max_linear_velocity=1000.0,
+            max_angular_velocity=1000.0,
+            max_depenetration_velocity=1.0,
+        ),
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=False, solver_position_iteration_count=4, solver_velocity_iteration_count=4
+        ),
+        joint_drive=sim_utils.UrdfConverterCfg.JointDriveCfg(
+            gains=sim_utils.UrdfConverterCfg.JointDriveCfg.PDGainsCfg(stiffness=0, damping=0)
+        ),
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        pos=(0.0, 0.0, 0.9),
+        joint_pos={
+            ".*hip_.*": 0.0,
+            ".*_knee_.*": 0.4,
+            ".*ankle_.*": -0.2,
+        },
+        joint_vel={".*": 0.0},
+    ),
+    soft_joint_pos_limit_factor=0.9,
+    actuators={
+        "actuators": ImplicitActuatorCfg(
+            joint_names_expr=[".*"],
+            stiffness={
+                ".*hip_.*": 200.0,
+                ".*knee_.*": 200.0,
+                ".*ankle_.*": 40.0,
+            },
+            damping={
+                ".*hip_.*": 4.0,
+                ".*knee_.*": 4.0,
+                ".*ankle_.*": 1.0,
+            },
+        ),
+    },
+)
+"""Configuration for the BDX humanoid robot."""
